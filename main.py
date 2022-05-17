2be4b2d08a67316bea13a092f06cd490c387551d
ronit450 <77673121+ronit450@users.noreply.github.com>
2022-05-18 02:29:35 +0500

    code at last night

--- main.py
+++ main.py
@@ -4,60 +4,43 @@ import os
 from tkinter import ttk
 from PIL import ImageTk, Image
 import tkinter as tk
+from tkinter.ttk import Combobox
 # from tkSliderWidget import Slider
 from RangeSlider.RangeSlider import RangeSliderH
 
-class frontend():
+class Login():
     def  __init__(self, main_root):
         self.main_root = main_root
 
+        # frame = Frame(self.main_root)
+        # frame.pack()
+
         self.main_image = ImageTk.PhotoImage (Image.open (
-            self.AbsolutePath('pictures_used/background1.png')))
+            self.AbsolutePath('pictures_used/loginpg_1366x720.png')))
         temp_label = Label (main_root,
                             image=self.main_image)
         temp_label.place (x=0,y=0, relwidth=1,relheight=1)
-        self.asking_parameter()
 
-    def slider_of_text(self):
-        pass
+        text_for_name = Text(self.main_root, bg = "#292B37", width = 30, font="Verdana 18",  height = 2)
+        # text_for_name.grid (padx=830,pady=310)
+        text_for_password = Text(self.main_root, bg = "#292B37", width = 31, font="Verdana 18",  height = 2)
+        # text_for_password.grid(padx = 40, pady = 430)
 
-    def asking_parameter(self):
+        Button (main_root,
+                text = "Login",
+                height=2,
+                width = 35,
+                bg='#1C1C1C',
+                command = lambda: self.homepage(),
+                borderwidth=2).grid (padx = 870, pady = 550)
 
-
-        self.genre_selctor()
-        self.imdb_slider()
-    def genre_selctor(self):
-        combostyle = ttk.Style ()
-
-        combostyle.theme_create ('combostyle',
-                                 parent='alt',
-                                 settings={'TCombobox':
-                                               {'configure':
-                                                    {
-                                                     'fieldbackground': '#292B37',
-
-                                                     }}}
-                                 )
-        # ATTENTION: this applies the new style 'combostyle' to all ttk.Combobox
-        combostyle.theme_use ('combostyle')
-
-        myframe1 = Frame(self.main_root)
-        myframe1.grid(padx = 146, pady = 440)
-        menue_items = [ ('Comedy'), ('Action'), ('Love'), ('Thriller')]
-        mycombo = ttk.Combobox(myframe1, value= menue_items, state = 'readonly', font="Verdana 18",  height=60,
-                               width=30,)
-        mycombo.current(0)
-
-        mycombo.pack()
-
-    def imdb_slider(self):
-        myslider2 = Scale (self.main_root,
-                           from_=0,
-                           to=100,
-                           orient=HORIZONTAL,
-                           width= '40')
-        # myslider2.set(34)
-        myslider2.grid ()
+    def homepage(self):
+        homepage = Toplevel ()
+        homepage.title ('HU Movie Akinator')
+        homepage.geometry ("1366x720")
+        homepage.config (bg='White')
+        homepage_obj = Homepage(homepage)
+        homepage.mainloop ()
 
 
 
@@ -68,9 +51,135 @@ class frontend():
         return returnDir
 
 
+
+
+
+class Homepage():
+    def __init__(self, homepage):
+        self.homepage = homepage
+        self.max = True
+        self.top = 45
+        self.parameter = "Name"
+        self.main_image = ImageTk.PhotoImage (Image.open (
+            self.AbsolutePath('pictures_used/finalhomepage.png')))
+        temp_label = Label (self.homepage,
+                            image=self.main_image)
+        temp_label.place (x=0,y=0, relwidth=1,relheight=1)
+        self.asking_parameter()
+        self.select_order_of_data()
+        self.selct_range_of_data()
+        self.proceed_button()
+        proceed_button = Button (self.homepage,
+                                 text="Proceed",
+                                 fg='black',
+                                 height=2,
+                                 width=35,
+                                 command=lambda: self.proceed_button(),
+                                 bg='light green')
+        proceed_button.place(x = 400, y = 600)
+
+
+    def proceed_button(self):
+        max_or_min = self.max
+        top_range = self.top
+        print(top_range)
+        print(max_or_min)
+        print(self.parameter)
+
+
+
+    def AbsolutePath (self,dir):
+            AbsoluteDirectory = os.path.dirname (os.path.abspath (__file__))
+            returnDir = os.path.join (AbsoluteDirectory,dir)
+            return returnDir
+
+    def asking_parameter (self):
+        value = self.genre_selctor ()
+        # self.imdb_slider ()
+
+    def genre_selctor (self):
+        combostyle = ttk.Style ()
+        combostyle.theme_create ('combostyle',parent='alt',settings={'TCombobox':{'configure':{'fieldbackground': '#292B37',}}})
+
+        combostyle.theme_use ('combostyle')
+        self.current_table = tk.StringVar ()
+        self.myframe1 = Frame (self.homepage, padx= 40, pady = 10,bg='#151718', borderwidth=0)
+        self.myframe1.place (x=40,y=200)
+        menue_items = [('Name'),('Genre'), ('IMDB Rating'), ('Year'), ('Duration')]
+        self.lbl = Label (self.myframe1,
+                          text="Select the Range of Data",
+                          fg='Black',
+                          font=("Helvetica", 14))
+        self.lbl.grid(padx = 10, pady = 10)
+        self.var = StringVar ()
+        self.var.set ("All")
+        self.cb = Combobox (self.myframe1,values=menue_items)
+        self.cb.bind ("<<ComboboxSelected>>",self.parameter_combo)
+        self.cb.grid()
+
+        btn = Button(self.myframe1,
+                          text="Get the Parameters")
+        btn.grid(padx = 50, pady = 10)
+
+        choice = self.current_table.get()
+    def parameter_combo(self,x):
+        self.parameter = self.cb.get()
+
+
+    def maxtomin(self):
+        self.max = True
+
+    def mintomax(self):
+        self.max = False
+
+    def select_order_of_data(self):
+        self.frame_for_order_of_data = Frame(self.homepage, padx= 40, pady = 20,bg='#151718', borderwidth=0)
+        self.frame_for_order_of_data.place (x=40,y=420)
+        self.lbl = Label (self.frame_for_order_of_data,
+                          text="Select the Order of Data",
+                          fg='Black',
+                          font=("Helvetica", 14))
+        self.lbl.grid(padx = 10, pady = 2)
+        self.v = IntVar ()
+        self.v.set (1)
+        self.r1 = Radiobutton (self.frame_for_order_of_data,text="Max to Min",variable=self.v,value=1,command=self.maxtomin)
+        self.r2 = Radiobutton (self.frame_for_order_of_data,text="Min to Max",variable=self.v,value=2,command=self.mintomax)
+        self.r1.grid()
+        self.r2.grid()
+
+    def combobox(self, x):
+        self.top = self.cb.get()
+        self.top = self.top[4:]
+        self.top = int(self.top)
+
+    def selct_range_of_data(self):
+        self.range_data_frame = Frame(self.homepage,  padx= 40, pady = 20,bg='#151718', borderwidth=0)
+        self.range_data_frame.place (x=350,y=420)
+        self.lbl = Label (self.range_data_frame,
+                          text="Select the Range of Data",
+                          fg='Black',
+                          font=("Helvetica", 14))
+        self.lbl.grid(padx = 10, pady = 10)
+        self.var = StringVar ()
+        self.var.set ("All")
+        self.data = ("top 5", "top 15", "top 30")
+        self.cb = Combobox (self.range_data_frame,values=self.data)
+        self.cb.bind ("<<ComboboxSelected>>",self.combobox)
+        self.cb.grid()
+
+    def imdb_slider (self):
+        myslider2 = Scale (self.homepage,
+                           from_=0,
+                           to=100,
+                           orient=HORIZONTAL,
+                           width='40')
+        # myslider2.set(34)
+        myslider2.grid ()
+
+
 main_root = Tk()
 main_root.title ('HU Movie Akinator')
 main_root.geometry ("1366x720")
 main_root.config (bg='White')
-obj1 = frontend(main_root)
+obj1 = Login(main_root)
 main_root.mainloop()
--- temp.py
+++ temp.py
@@ -1,28 +1,115 @@
+from distutils.command import register
+from doctest import master
 from tkinter import *
-import time
-# Create a tkinter window
-root = Tk()
-root.title("Label Slider")
-root.geometry("500x200")
-char_index = 0
-text = " "
-slide_text = "www.codershubb.com"
-# function to display text characters with
-# a particular time interval
-def slide():
-    global char_index , text, slide_text
-    if char_index >= len(slide_text):
-        char_index = 0
-        text = " "
-        slider.config(text = text)
-    else:
-        text = text + slide_text[char_index]
-        slider.configure(text = text)
-        char_index = char_index + 1
-    slider.after(500, slide)
-# place label on a window
-slider = Label(root, text = text, font=("Arial", 25, "bold"), width=20)
-slider.place(x = 30 , y = 60)
-# call the function
-slide()
-root.mainloop()
+from tkinter import messagebox
+
+
+class User():
+    def __init__(self, username, password, hobbies):
+        self.username = username
+        self.password = password
+        self.hobbies = []
+        for i in hobbies:
+            self.hobbies.append(i)
+class Authentication():
+
+    def __init__(self):
+        self.list_of_record = dict()
+
+    def click(self, username, password, hobbies):
+        user1 = User(username, password, hobbies)
+        self.list_of_record[user1.username] = user1
+
+    def check(self, username, password):
+        found = self.list_of_record.get(username, False)
+
+        if found:
+            # print(found.password)
+            if password == found.password:
+                return ("Password is correct..... Login Successful", found)
+            else:
+                return ("Incorrect password", found)
+
+        else:
+            return ("No username of this ID was foud", found)
+
+
+authentication = Authentication()
+authentication.click("oqba", "abdul", ["fishing", "farming"])
+class loginpage(Frame):  # login page
+    def __init__(self, root):
+        Frame.__init__(self, root)
+        # this will create a window and between this and main loop we have to work for GUI
+        self.root = root
+
+        self.authentication = Authentication()
+        self.authentication.click("oqba", "abdul", ["fishing", "farming"])
+        self.root.title("Login Page")
+        self.root.geometry('1280x720')
+        self.root.resizable(False, False)
+        # register_frame=Frame(self.root, borderwidth=2, relief='sunken', bg="white", bd='10')
+        # register_frame.place(x=0, y=0,height = 720, width =1280 )
+        # self.b = PhotoImage(file = "login.png")
+        # Label(register_frame, image=self.b).place(x=0, y=0, relwidth = 1, relheight = 1)
+
+        def Message(Text):
+            messagebox.showinfo("Message", Text)
+
+        # canvas1 = Canvas(self.root, width = 1280, height = 720)
+        # self.b = PhotoImage(file = "login.png")
+        # canvas1.create_image( 0, 0, image = self.b)
+        # Label(self.root, image=self.b, width='2000', height='4000').pack()
+
+        login_frame = Frame(self.root, borderwidth=2,
+                            relief='sunken', bg="white", bd='2')
+        login_frame.place(x=0, y=0, height=720, width=1280)
+
+        canvas1 = Canvas(login_frame, width=1280, height=720)
+        self.b = PhotoImage(file="loginpg_1366x720.png")
+        canvas1.create_image(0, 0, image=self.b)
+        Label(login_frame, image=self.b, width='1280', height='720').pack()
+
+        # label = Label(login_frame,text="Login Here", font=("Bebas Neue", 35, "bold"), fg='#405898', bg="white").place(x=125, y=0)
+        # label1 = Label(login_frame,text="Username", font=("Goudy old style", 15, "bold"), fg="gray", bg="white").place(x=50, y=120)
+        # --> create an Entry for user to write his name
+        self.E1 = Entry(login_frame, bd=0, bg="white")
+        self.E1.place(x=780, y=280, width=245, height=30)
+
+        # label2 = Label(login_frame,text="Password", font=("Goudy old style", 15, "bold"), fg="gray", bg="white").place(x=50, y=170)
+        # --> create an Entry for user to write his name
+        self.E2 = Entry(login_frame, bd=0, bg="white")
+        self.E2.place(x=780, y=377, width=245, height=30)
+
+        self.b1 = PhotoImage(file="registershape.png")
+        signup_button = Button(login_frame, text='SignUp', command='c', image=self.b1, relief='sunken', padx=5, pady=5,
+                               activebackground='white', activeforeground='red', bg='white', fg='black', font=('Bebas Neue', 15), bd=0, height='30', width='120')
+        signup_button.place(x=837, y=507)
+
+        def _login():
+            user = self.E1.get()
+            password = self.E2.get()
+            if self.authentication.check(user, password)[0] == "Password is correct..... Login Successful":
+
+                print(self.authentication.check(user, password)[1])
+                login_frame.destroy()
+                self.post_frame = Frame(
+                    self.root, borderwidth=2, relief='sunken', bg="white", bd='10')
+                self.post_frame.place(x=0, y=0, height=720, width=1280)
+                # back_button=Button(self.post_frame, text='Back',command=goback, padx=5, pady=5,  activebackground='light blue', activeforeground='red', bg='#778899', fg='white', font=('Bebas Neue', 15, 'bold'), bd=5)
+                # back_button.place(x=170, y=300)
+
+                # currentData = Data()
+                BG_GRAY = "#ABB2B9"
+                BG_COLOR = "#17202A"
+                TEXT_COLOR = "#EAECEE"
+
+
+                FONT = "Helvetica 14"
+                FONT_BOLD = "Helvetica 13 bold"
+
+main_root = Tk()
+main_root.title ('HU Movie Akinator')
+main_root.geometry ("1366x720")
+main_root.config (bg='White')
+obj1 = loginpage(main_root)
+main_root.mainloop()
